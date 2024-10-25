baa9ffff29b6517f6d6a82b0cd6811e31a9d92b9
Kwizera15 <kwizerad523@gmail.com>
2024-10-25 15:35:58 +0200

    Add files via upload

--- Grading.py
+++ Grading.py
@@ -1,1 +1,14 @@
-
+a=int(input("Enter any number:"))
+if a>70:
+    grade="A"
+elif a>=60:
+    grade="B"
+elif a>=50:
+        grade="C"
+            
+elif a>=40:
+    grade="D"
+ 
+else :
+    grade="fail"
+print(f'your grade marks is:{grade}')
